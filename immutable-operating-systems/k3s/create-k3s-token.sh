32d01085092e78e36bd509374e306bfe87ca6f8d
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-01-12 10:30:10 +0100

    fix token creation

--- immutable-operating-systems/k3s/create-k3s-token.sh
+++ immutable-operating-systems/k3s/create-k3s-token.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-TMP_K3S_PATH="/tmp/k3s"
-mkdir -p $TMP_K3S_PATH
-K3S_VERSION=v1.28.5+k3s1
-wget -nc -O $TMP_K3S_PATH/k3s https://github.com/k3s-io/k3s/releases/download/$K3S_VERSION/k3s
-chmod +x $TMP_K3S_PATH/k3s
-
-$TMP_K3S_PATH/k3s token generate > k3s/k3s_token
+cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1 > k3s/k3s_token
 cp k3s/k3s_token microos/combustion-ignition/combustion/k3s_token
