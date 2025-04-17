bde2ce446e1a969b6f12afc8ebe846ca922fe66a
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-04-17 14:36:59 +0200

    add further packages

--- pulceo-data-analytics/app/pda.R
+++ pulceo-data-analytics/app/pda.R
@@ -1,11 +1,38 @@
 # plumber.R
 library(plumber)
 library(rmarkdown)
+library(jsonlite)
+
+
 
 #* @get /render
 function(req, res) {
-    # input <- jsonlite::fromJSON(req$postBody)
-    # rmd_path <- input$rmd_path # e.g., "template.Rmd"
+    # input <- fromJSON(req$postBody)
+
+    # TODO: the orchestration id
+    SUBFOLDER <- "summersoc2025"
+    dir.create(paste("raw", SUBFOLDER, sep = "/"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste("reports", SUBFOLDER, sep = "/"), recursive = TRUE, showWarnings = FALSE)
+    # TODO: get resources and put in raw
+    tasks_uri <- paste0("https://raw.githubusercontent.com/spboehm/pulceo-misc/refs/heads/feature/1mn-rework/pulceo-data-analytics/raw/", SUBFOLDER, "/TASKS.csv")
+    events_uri <- paste0("https://raw.githubusercontent.com/spboehm/pulceo-misc/refs/heads/feature/1mn-rework/pulceo-data-analytics/raw/", SUBFOLDER, "/EVENTS.csv")
+    requests_uri <- paste0("https://raw.githubusercontent.com/spboehm/pulceo-misc/refs/heads/feature/1mn-rework/pulceo-data-analytics/raw/", SUBFOLDER, "/REQUESTS.csv")
+
+    # TODO: curl_tasks
+    download.file(tasks_uri, paste("raw", SUBFOLDER, "TASKS.csv", sep = "/"))
+    # TODO: curl_events
+    download.file(events_uri, paste("raw", SUBFOLDER, "EVENTS.csv", sep = "/"))
+    # TODO: curl_requests
+    download.file(requests_uri, paste("raw", SUBFOLDER, "REQUESTS.csv", sep = "/"))
+
+    rmd <- "pulceo-data-analytics-offloading.Rmd"
+    rmarkdown::render(rmd,
+        params = list(subfolder = SUBFOLDER),
+        output_dir = paste0("reports", "/", subfolder = SUBFOLDER),
+        output_file = "index.html"
+    )
+
+
     # output_path <- tempfile(fileext = ".html")
 
     # render(rmd_path, output_file = output_path, params = input$params)
--- pulceo-data-analytics/install.R
+++
@@ -1,2 +0,0 @@
-# install.R
-install.packages("rmarkdown", repos = "http://cran.us.r-project.org")
