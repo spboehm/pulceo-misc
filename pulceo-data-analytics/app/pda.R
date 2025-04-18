cd2749b9a10b9355e44ff675001ca9b8aa7ae6ed
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-04-18 12:59:33 +0200

    add /logs endpoint

--- pulceo-data-analytics/app/pda.R
+++ pulceo-data-analytics/app/pda.R
@@ -21,3 +21,29 @@ function(req, res) {
     res$body <- "OK"
     return(res)
 }
+
+#* @get /logs/<orchestrationId>
+function(orchestrationId, res) {
+    if (missing(orchestrationId)) {
+        res$status <- 400
+        res$body <- "Missing orchestrationId"
+        return(res)
+    }
+    print(orchestrationId)
+    log_file <- paste0("pda-worker-logs/", orchestrationId, ".log")
+    if (file.exists(log_file)) {
+        logs <- readLines(log_file)
+    } else {
+        logs <- character(0)
+    }
+
+    if (length(logs) == 0) {
+        res$status <- 404
+        res$body <- "No logs found for the given orchestrationId"
+        return(res)
+    }
+
+    res$status <- 200
+    res$body <- toJSON(logs)
+    return(res)
+}
