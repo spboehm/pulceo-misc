42a4360dadecbe9055e6051116107c7021c636f5
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-06-15 22:38:35 +0200

    fix combined util

---
+++ pulceo-data-analytics/includes/resources/metrics/cpu-util-applications.R
@@ -0,0 +1,27 @@
+source(here("includes/resources/metrics/cpu-util.R"))
+
+CPU_UTIL_PODS <- filterResourceUtilForNode(CPU_UTIL, "usageCPUPercentage", "POD", "traefik|pulceo-node-agent|edge-iot-simulator")
+
+CPU_UTIL_PODS_PROCESSED <- CPU_UTIL_PODS %>%
+  group_by(nodeName) %>%
+  arrange(timestamp, .by_group = TRUE) %>%
+  mutate(id = row_number()) %>%
+  mutate(application = if_else(grepl("traefik", resourceName), "traefik",
+    if_else(grepl("pulceo-node-agent", resourceName), "pulceo-node-agent",
+      if_else(grepl("edge-iot-simulator", resourceName), "edge-iot-simulator", "")
+    )
+  ))
+
+CPU_UTIL_pods_summary <- CreateSummary(filterResourceUtilForNode(CPU_UTIL, "usageCPUPercentage", "POD", "edge-iot-simulator"), "Pod")
+
+CPU_UTIL_PODS_SUMMARY_BY_APP <- CPU_UTIL %>%
+  filter(k8sResourceType == "POD", X_field == "usageCPUPercentage") %>%
+  group_by(nodeName, resourceName) %>%
+  summarize(
+    min = round(min(X_value), 3),
+    avg = round(mean(X_value), 3),
+    max = round(max(X_value), 3),
+    median = round(median(X_value), 3),
+    sd = round(sd(X_value), 3), 
+    .groups = "drop"
+  )
--- pulceo-data-analytics/includes/resources/metrics/net-util.R
+++ pulceo-data-analytics/includes/resources/metrics/net-util.R
@@ -3,24 +3,3 @@ source(here("includes/R/filter.R"))
 source(here("includes/resources/meta.R"))
 NET_UTIL_RAW <- ReadAndFilterTimestamp(here(paste("raw", SUBFOLDER, "NET_UTIL.csv", sep = "/")), timestamp)
 NET_UTIL <- TransfromNodeMetricsMetadata(NET_UTIL_RAW)
-
-# TODO: HasNodes, HasApplications?
-# TODO: Remove?
-INITIAL_TX_RX <- NET_UTIL %>%
-  group_by(nodeName, X_field, k8sResourceType, resourceName) %>%
-  slice(which.min(timestamp)) %>%
-  select(nodeName, sourceHost, k8sResourceType, resourceName, X_field, X_value) %>%
-  rename(X_value_initial = X_value)
-
-COMBINED_NET_UTIL <- left_join(NET_UTIL, INITIAL_TX_RX, join_by(nodeName, sourceHost, k8sResourceType, resourceName, X_field))
-COMBINED_NET_UTIL$X_value_final <- ((COMBINED_NET_UTIL$X_value - COMBINED_NET_UTIL$X_value_initial))
-
-COMBINED_NET_UTIL_POD <- rbind(filterResourceUtilForNode(COMBINED_NET_UTIL, "rxBytes", "POD", "traefik|pulceo-node-agent|edge-iot-simulator"), filterResourceUtilForNode(COMBINED_NET_UTIL, "txBytes", "POD", "traefik|pulceo-node-agent|edge-iot-simulator"))
-
-COMBINED_NET_UTIL_POD_PROCESSED <- COMBINED_NET_UTIL_POD %>%
-  mutate(application = if_else(grepl("traefik", resourceName), "traefik",
-    if_else(grepl("pulceo-node-agent", resourceName), "pulceo-node-agent",
-      if_else(grepl("edge-iot-simulator", resourceName), "edge-iot-simulator", "")
-    )
-  )) %>%
-  mutate(field = if_else(grepl("rxBytes", X_field), "Received (Rx)", if_else(grepl("txBytes", X_field), "Transmitted (Tx)", "")))
