bceed641d7320da9d7bcee080c5a1d06680894dc
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-06-22 20:13:39 +0200

    update dir structure

---
+++ pulceo-data-analytics/includes/resources/metrics/rtt-icmp.R
@@ -0,0 +1,15 @@
+source(here("includes/resources/metrics/rtt.R"))
+
+ICMP_RTT_SUMMARY <- filterICMPRTT(ICMP_RTT, "rttAvg")
+
+ICMP_RTT_SUMMARY <- ICMP_RTT_SUMMARY %>%
+  group_by(nodeName, nodeNameDest) %>%
+  summarise(
+    min = round(min(X_value), 3),
+    avg = round(mean(X_value), 3),
+    max = round(max(X_value), 3),
+    median = round(median(X_value), 3),
+    sd = round(sd(X_value), 3), .groups = "drop"
+  ) %>%
+  mutate_if(is.numeric, scale, na.rm = TRUE)
+names(ICMP_RTT_SUMMARY) <- c("$v_1$", "$v_2$", "Min", "Mean", "Max", "Med", "SD")
