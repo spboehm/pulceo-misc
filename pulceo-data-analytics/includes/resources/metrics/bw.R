a70c143de22230509c048bfa9bf53f08829432b0
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-07-02 15:16:28 +0200

    fix assignment

--- pulceo-data-analytics/includes/resources/metrics/bw.R
+++ pulceo-data-analytics/includes/resources/metrics/bw.R
@@ -35,7 +35,7 @@ if (TCP_BW_PRESENT) {
 # UDP
 tryCatch({
   UDP_BW_RAW <- ReadAndFilterEndTime(paste(FOLDER_PFX_RAW, "UDP_BW.csv", sep = "/"), endTime)
-  UDP_BW <<- TransfromNetworkMetricsMetadata(UDP_BW_RAW)
+  UDP_BW <- TransfromNetworkMetricsMetadata(UDP_BW_RAW)
 }, error = function(e) {
     message("Error loading or transforming UDP_BW: ", e$message)
     UDP_BW <<- data.frame()
