823cfafbe8fa7637e5b4895334f5e6583017406d
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-07-03 14:26:41 +0200

    fix display of nodes

--- pulceo-data-analytics/includes/R/tables.R
+++ pulceo-data-analytics/includes/R/tables.R
@@ -2,11 +2,14 @@ source(here("includes/R/libraries.R"))
 
 install_and_load("kableExtra")
 install_and_load("DT")
+install_and_load("tidyverse")
 
 
-CreateScrollableTable <- function(df) {
-  (kbl(df) %>% scroll_box(width = "100%", height = "400px") %>% kable_styling(bootstrap_options = c("striped", "hover", "condensed")))
-  #CreatePaginatedTableDT(df)
+CreateScrollableTable <- function(df, removeCols = NULL) {
+  if (!is.null(cols)) {
+    df <- select(df, -all_of(removeCols))
+  }
+  CreatePaginatedTableDT(df)
 }
 
 CreatePaginatedTableDT <- function(df, pageLength = 10) {
