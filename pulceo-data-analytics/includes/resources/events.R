a28722c445f0b7e6d55a6f8d37af2703dc8bb2c2
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-07-17 11:40:49 +0200

    update file path of events

--- pulceo-data-analytics/includes/resources/events.R
+++ pulceo-data-analytics/includes/resources/events.R
@@ -1,7 +1,13 @@
 source(here('includes/R/filter.R'))
 
+EVENTS_FILE_PATH <- paste(FOLDER_PFX_RAW, "EVENTS.csv", sep = "/")
+
+if (!file.exists(EVENTS_FILE_PATH)) {
+    EVENTS_FILE_PATH <- paste(FOLDER_PFX_RAW, "EVENT.csv", sep = "/")
+}
+
 tryCatch({
-    EVENTS_RAW <- read.csv(paste(FOLDER_PFX_RAW, "EVENTS.csv", sep = "/"), skip = 3)
+    EVENTS_RAW <- read.csv(EVENTS_FILE_PATH, skip = 3)
 }, error = function(e) {
     message("Error in read EVENTS.csv: ", e$message)
     EVENTS_RAW <<- data.frame()
